63d45be5e4ddf5ee22839cd61ffeae2c87aaa7b4
smarbos <smarbos@gmail.com>
2016-05-07 15:15:45 -0300

    one liner include

--- adopteitor_core/admin.py
+++ adopteitor_core/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 
-from adopteitor_core.models import Animal
-from adopteitor_core.models import AnimalFoto
-from adopteitor_core.models import FormularioAdopcion
-from adopteitor_core.models import Persona
+from adopteitor_core.models import Animal, AnimalFoto, FormularioAdopcion, Persona
 
 admin.site.register(Animal)
 admin.site.register(AnimalFoto)
