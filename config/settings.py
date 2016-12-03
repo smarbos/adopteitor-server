a090ca59b22f3a6cd3356a33c1299fb0c88f2f8e
Damian Mantuano <damianm@camba.coop>
2016-12-03 20:14:10 -0300

    commented secutity middoleware

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware'
 )
